cdfeae728767cca451092b0ef23507f2f7f5dde6
mabi0110 <mariadb0110gmail.com>
2026-05-13 16:01:03 +0200

    add xml as expected response format

--- src/main/java/pl/javastart/api/BookController.java
+++ src/main/java/pl/javastart/api/BookController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/books")
+@RequestMapping(value = "api/books", produces = "application/xml")
 public class BookController {
 
     @GetMapping()
