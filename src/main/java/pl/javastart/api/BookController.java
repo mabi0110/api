c0be26ca3f3a22c52cba328728968e81de176fb2
mabi0110 <mariadb0110gmail.com>
2026-05-13 13:26:16 +0200

    add @RequestMapping annotation

--- src/main/java/pl/javastart/api/BookController.java
+++ src/main/java/pl/javastart/api/BookController.java
@@ -1,14 +1,16 @@
 package pl.javastart.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/books")
 public class BookController {
 
-    @GetMapping("api/books")
+    @GetMapping()
     List<Book> getAllBooks() {
         return List.of(
                 new Book("Harry Potter i Kamień Filozoficzny", "J.K. Rowling", 199732323, 223),
@@ -16,7 +18,7 @@ public class BookController {
                 new Book("Harry Potter i Zakon Feniksa", "J.K. Rowling", 199732325, 453));
     }
 
-    @GetMapping("api/books/1")
+    @GetMapping("/1")
     Book getSingleBook() {
         return new Book("Harry Potter i Kamień Filozoficzny", "J.K. Rowling", 199732323, 223);
     }
