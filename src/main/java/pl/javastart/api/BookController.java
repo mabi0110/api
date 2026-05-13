3011a435409a56d37afb0d3c7462b8b47bfc2e08
mabi0110 <mariadb0110gmail.com>
2026-05-13 16:02:18 +0200

    add json and xml as expected response format

--- src/main/java/pl/javastart/api/BookController.java
+++ src/main/java/pl/javastart/api/BookController.java
@@ -1,5 +1,6 @@
 package pl.javastart.api;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/books", produces = "application/xml")
+@RequestMapping(value = "api/books", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class BookController {
 
     @GetMapping()
